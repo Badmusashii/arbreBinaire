57b268e213e259066f68ea7b13502198b3fb6ddf
Badmusashii <badmusashii@gmail.com>
2024-02-08 15:22:10 +0100

    ajout de nouvelle recherche

--- index.js
+++ index.js
@@ -110,8 +110,46 @@ const breadthFirstValues = (root) => {
   return values;
 };
 
+/**
+ * Recherche une valeur cible dans un arbre binaire en utilisant un parcours en largeur (Breadth-First Search).
+ * Cette méthode itérative utilise une file d'attente pour parcourir l'arbre niveau par niveau,
+ * en commençant par la racine et en poursuivant avec les nœuds enfant de gauche à droite.
+ *
+ * @param {Node} root - La racine de l'arbre binaire à parcourir.
+ * @param {*} target - La valeur cible à rechercher dans l'arbre.
+ * @returns {boolean} Retourne true si la valeur cible est trouvée, false autrement.
+ */
+const treeIncludes = (root, target) => {
+  if (root === null) return false;
+  const queue = [root];
+  while (queue.length > 0) {
+    const current = queue.shift();
+    if (current.value === target) return true;
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return false;
+};
+
+/**
+ * Recherche une valeur cible dans un arbre binaire en utilisant un parcours en profondeur récursif.
+ * Cette méthode examine chaque nœud en commençant par la racine, puis parcourt récursivement
+ * les sous-arbres gauche et droit, en cherchant la valeur cible.
+ *
+ * @param {Node} root - Le nœud racine de l'arbre binaire ou d'un sous-arbre actuel.
+ * @param {*} target - La valeur cible à rechercher dans l'arbre.
+ * @returns {boolean} Retourne true si la valeur cible est trouvée dans l'arbre, false sinon.
+ */
+const treeIncludes2 = (root, target) => {
+  if (root === null) return false;
+  if (root.value === target) return true;
+  return treeIncludes2(root.left, target) || treeIncludes2(root.right, target);
+};
+
 // Appels de fonctions pour tester les implémentations des parcours
 depthFirstValues(A);
 console.log(depthFirstValues2(A));
 console.log(depthFirstValues3(A));
 console.log(breadthFirstValues(A));
+console.log(treeIncludes(A, "B"));
+console.log(treeIncludes2(A, "B"));
